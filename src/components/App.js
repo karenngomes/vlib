505d744440ff402b452ffd43b7f21d31f63007fe
Karen Gomes <kngs@ic.ufal.br>
2018-09-23 16:34:48 -0300

    estruturando projeto

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import Login from './Login';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="box">
+        <div className="box__middle">
+          <h1 className="box__title">VLib App</h1>
+          <p>Login</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
